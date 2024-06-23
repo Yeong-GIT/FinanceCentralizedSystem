c4859155dd0015a39d180f16029f94fd98f293cb
Admin-HL <yeonghaoliang0822@gmail.com>
2024-06-23 23:34:39 +0800

    Initial commit and config for Finance Centralized System

---
+++ src/test/java/FinanceSystem/FinanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package FinanceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
