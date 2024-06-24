d98c6d8b251aaafbea95764e22e18644cadce8d5
Admin-HL <yeonghaoliang0822@gmail.com>
2024-06-25 02:21:07 +0800

    feat(Kafka): Config service and kafka bugs. -Adding starter web dependency, application port and supporting code in main java to maintain the exited service.

--- src/main/java/FinanceSystem/FinanceSystemApplication.java
+++ src/main/java/FinanceSystem/FinanceSystemApplication.java
@@ -2,6 +2,8 @@ package FinanceSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FinanceSystemApplication {
@@ -10,4 +12,11 @@ public class FinanceSystemApplication {
 		SpringApplication.run(FinanceSystemApplication.class, args);
 	}
 
+	@RestController
+	class HealthCheckController {
+		@GetMapping("/health")
+		public String healthCheck() {
+			return "OK";
+		}
+	}
 }
